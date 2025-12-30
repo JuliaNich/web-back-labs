09dda2175f213f05416f961236233394cf0accbc
Ничипоренко <julianichi@mail.ru>
2025-12-30 14:48:29 +0700

    модель базы данных

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 import os
+from os import path
 from flask import Flask, url_for, request, redirect, make_response, abort, send_from_directory
 import datetime
+from db import db
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "секретно-секретный секрет")
-app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'sqlite')
+app.config['DB_TYPE'] = os.environ.get('DB_TYPE', 'postgres')  
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'julia_nichiporenko_orm'
+    db_user = 'julia_nichiporenko_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "julia_nichiporenko_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 from lab1 import lab1  
 from lab2 import lab2  
