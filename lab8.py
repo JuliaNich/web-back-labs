cf8a295792052f37cbf72d2673dfb29429286f90
Ничипоренко <julianichi@mail.ru>
2025-12-30 15:56:26 +0700

    регистрация

--- lab8.py
+++ lab8.py
@@ -12,3 +12,27 @@ lab8 = Blueprint('lab8', __name__)
 def lab():
     username = session.get('username', 'anonymous')
     return render_template('lab8/lab8.html', login=session.get('login'))
+
+@lab8.route('/lab8/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form or not password_form:
+        return render_template('lab8/register.html',
+                               error='Логин и пароль не могут быть пустыми')
+
+    login_exists = users.query.filter_by(login=login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html',
+                               error='Такой пользователь уже существует')
+
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login=login_form, password=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect('/lab8/')
